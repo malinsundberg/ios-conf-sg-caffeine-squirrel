e2529ac657a25406ba129ef7f7d1cb08946e3adb
Malin Sundberg <mail@malinsundberg.com>
2022-01-16 16:37:48 -0800

    Adds functionality for step six

--- iOS/Views/Insights Tab/ChartBar.swift
+++ iOS/Views/Insights Tab/ChartBar.swift
@@ -10,9 +10,14 @@ import SwiftUI
 struct ChartBar: View {
     let value: Double
     let color: Color
+    let isSelected: Bool
+    
+    private var scale: CGFloat {
+        isSelected ? 1.025 : 1.0
+    }
     
     private var linearGradientColor: LinearGradient {
-        LinearGradient(gradient: Gradient(colors: [color, color.opacity(0.4)]), startPoint: .bottom, endPoint: .top)
+        LinearGradient(gradient: Gradient(colors: [color, color.opacity(isSelected ? 0.8 : 0.4)]), startPoint: .bottom, endPoint: .top)
     }
     
     var body: some View {
@@ -20,6 +25,7 @@ struct ChartBar: View {
             RoundedCornerRectangle(topLeft: 12, topRight: 12, bottomLeft: 0, bottomRight: 0)
                 .fill(linearGradientColor)
                 .padding(.top, getInset(for: geo.size.height, value: value))
+                .scaleEffect(scale, anchor: .bottom)
         }
     }
     
@@ -30,7 +36,7 @@ struct ChartBar: View {
 
 struct ChartBar_Previews: PreviewProvider {
     static var previews: some View {
-        ChartBar(value: 0.7, color: Color.chartColor1)
+        ChartBar(value: 0.7, color: Color.chartColor1, isSelected: false)
     }
 }
 
--- iOS/Views/Insights Tab/InsightsView.swift
+++ iOS/Views/Insights Tab/InsightsView.swift
@@ -16,10 +16,25 @@ struct InsightsView: View {
         }
     }
     
+    private var beveragesHighlight: String {
+        let totalValue: Int
+        
+        if let selectedValue = selectedBeveragesValue,
+           let selectedDay = beverageStore.dailyBeverages.first(where: { $0.id == selectedValue.id }) {
+            totalValue = selectedDay.beverages.count
+        } else {
+            totalValue = beverageStore.totalBeverages
+        }
+
+        return totalValue == 1 ? "\(totalValue.formatted()) cup" : "\(totalValue.formatted()) cups"
+    }
+    
+    @State private var selectedBeveragesValue: ChartValue?
+    
     var body: some View {
         ScrollView {
             VStack(spacing: 12) {
-                ChartView(values: beverageCountValues, color: Color.chartColor1, yAxisName: "Beverages", xAxisName: "Days")
+                ChartView(values: beverageCountValues, color: Color.chartColor1, yAxisName: "Beverages", xAxisName: "Days", description: "Total", highlight: beveragesHighlight, selectedValue: $selectedBeveragesValue)
             }.padding()
         }.background(Color.backgroundColor)
     }
